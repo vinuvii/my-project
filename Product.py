223e76f4c2e3438edfa2fcebf4df46620b8e48ac
Vinuvi Nuhansa <vinuvidandeniya@gmail.com>
2024-05-17 00:34:55 +0530

    added getters and setters

---
+++ Product.py
@@ -0,0 +1,29 @@
+class Product:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, price):
+        self._price = price
+
+    @property
+    def quantity(self):
+        return self._quantity
+
+    @quantity.setter
+    def quantity(self, quantity):
+        self._quantity = quantity
