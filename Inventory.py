40121223cafc83445b819ef1c4facbf4f5dfd2ba
Vinuvi Nuhansa <vinuvidandeniya@gmail.com>
2024-05-17 13:06:33 +0530

    added a function addproduct

--- Inventory.py
+++ Inventory.py
@@ -9,3 +9,10 @@ class Inventory:
     @products.setter
     def products(self, products):
         self._products = products
+
+    def add_product(self, product, quantity):
+        if product.name in self._products:
+            self._products[product.name]['quantity'] += quantity
+        else:
+            self._products[product.name] = {'product': product, 'quantity': quantity}
+
